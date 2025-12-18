6ad8d0a40b2eb6dff5a147a961d4c1997c3afe55
Indra N. Utomo <Indra77ya@gmail.com>
2025-12-18 15:50:58 +0700

    update tenant url

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -375,7 +375,7 @@ config var as global var
 */
 $config['mymenu']='';
 $config['mysubmenu']='';
-$config['tenant_url']='http://sikos42tenant.amanahsolution.com/';
+$config['tenant_url']='https://simkos.hassapos.online/tenant/';
 
 
 
