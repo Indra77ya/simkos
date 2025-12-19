fe130e38081c2aeddc8dfca14a974d5d69fd10d8
Indra N. Utomo <Indra77ya@gmail.com>
2025-12-19 14:31:09 +0700

    update lebar kolom list kamar

--- admin/application/views/ftransaksi/frmlistkamar.php
+++ admin/application/views/ftransaksi/frmlistkamar.php
@@ -63,7 +63,7 @@
 			"responsive": true,
 			"autoWidth": false,
 			"columnDefs": [
-				{ "width": "4%", "targets": 0 },
+				{ "width": "2%", "targets": 0 },
 				{ "width": "10%", "targets": 1 },
 				{ "width": "10%", "targets": 2 },
 				{ "width": "6%", "targets": 3 },
