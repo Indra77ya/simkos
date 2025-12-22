226e568f2e88e4a0d58bdb2ca5b0da17f90cb7fc
Indra N. Utomo <Indra77ya@gmail.com>
2025-12-22 13:42:24 +0700

    delete two strip

--- admin/application/views/dashboard/index.php
+++ admin/application/views/dashboard/index.php
@@ -181,9 +181,7 @@
 												
 											</div>
 
-											<div class="space-20"></div>
-											<div class="hr hr2 hr-double"></div>
-											<div class="space-6"></div>
+											
 											
 										</div>
 									</div>
